c965bb85009bcda0c3874032e1325098801967bb
Sahebpatra <sahebpatra30@gmail.com>
2022-06-05 13:41:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentMVC.Models;
+
+namespace StudentMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        LinqAssinmentEntities db = new LinqAssinmentEntities();
+
+        public ActionResult Index()
+        {
+            var s = db.students;
+            return View(s);
+        }
+    }
+}
